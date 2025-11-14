0efb1f2acfbe80822f4779ed794cb6b2b37d1831
hamza Dazia <135356014+HamzaDazIA@users.noreply.github.com>
2025-11-14 16:12:34 +0000

    Ss

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,51 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45)
+{
+    this->setTarget("Default");
+}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("RobotomyRequestForm", 72, 45)
+{
+    this->setTarget(target);
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &obj) : AForm(obj)
+{
+    this->setTarget(obj.getTarget());
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &other)
+{
+    if (this != &other)
+    {
+        AForm::operator=(other);
+        this->setTarget(other.getTarget());
+    }
+    return *this;
+}
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+const char *RobotomyRequestForm::Faildrobotomy::what() const throw()
+{
+    return ("That the robotomy failed");
+}
+void RobotomyRequestForm::execute(Bureaucrat const & executor) const
+{
+    if (this->getSignd() == false)
+    {
+        throw RobotomyRequestForm::FormIsNotSigndException();
+    }
+
+    if (this->getExecSignd() < executor.getGrade())
+        throw RobotomyRequestForm::GradeTooLowException();
+    if (std::rand() > 50)
+    {
+        throw::RobotomyRequestForm::Faildrobotomy();
+    }
+    else if (std::rand() < 50)
+    {
+        std::cout << "DRRRRIIIILLLL..." << std::endl;
+    }
+
+}
---
+++ ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,22 @@
+#ifndef RobotomyRequestForm_HPP
+#define RobotomyRequestForm_HPP
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm: public AForm
+{
+    public:
+        RobotomyRequestForm();
+        RobotomyRequestForm(std::string target);
+        RobotomyRequestForm(const RobotomyRequestForm& obj);
+        ~RobotomyRequestForm();
+        RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+        void execute(Bureaucrat const & executor) const;
+        class Faildrobotomy : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};
+
+#endif
